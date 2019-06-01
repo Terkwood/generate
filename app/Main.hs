29daf83c32ef85aadafa5ce0acd84b8731f48f49
Payton Turnage <paytonturnage@gmail.com>
2019-06-01 16:56:59 -0700

    Hair work.

--- app/Main.hs
+++ app/Main.hs
@@ -17,50 +17,119 @@ import Petal
 ramp :: Int -> V.Vector Double
 ramp n = V.generate n $ \i -> fromIntegral i / fromIntegral (n - 1)
 
-gridPetals :: Int -> THColours -> Generate [Petal]
-gridPetals layerCount palette = do
+data PetalSpawnPoint = PetalSpawnPoint
+  { _petalSpawnPointPoint :: V2 Double
+  , _petalSpawnPointTheta :: Double
+  , _petalSpawnPointThickness :: Double
+  }
+
+waterPetals :: Int -> [PetalSpawnPoint] -> THColours -> Generate [Petal]
+waterPetals layerCount spawnPoints palette = do
   World {..} <- asks world
-  count :: Int <- sampleRVar $ uniform 10000 20000
-  screen <- fullFrame
-  let screen' = scaleFrom 1.2 (center screen) screen
-  spawnPoints <-
-    sequence $ map (const $ spatialSample screen') [1 .. (count * 50)]
-  noiseScale <- sampleRVar $ uniform (width / 3 * 2) (width * 2)
-  noiseSamples <-
-    sequence $
-    map
-      (\(V2 x y) -> noiseSample $ V3 (x / noiseScale) (y / noiseScale) 0.4)
-      spawnPoints
-  let thetas = map (* (2 * pi)) noiseSamples
-  baseSize <- sampleRVar $ uniform 30 70
-  sizes <-
-    sequence $
-    map
-      (const $ sampleRVar (normal 0 3 >>= return . (+ baseSize) . abs))
-      spawnPoints
+  let baseSize = maximum $ map (_petalSpawnPointThickness) spawnPoints
   petals <-
     sequence $
-    map (\(s, p, t) -> mkPetal palette s p t) $ zip3 sizes spawnPoints thetas
-  wigglePower <- sampleRVar (normal 0 $ (baseSize / 10)) >>= return . abs
+    map (\(PetalSpawnPoint p t s) -> mkPetal palette s p t) spawnPoints
+  wigglePower <-
+    sampleRVar (uniform (baseSize / 10) (baseSize / 6)) >>= return . abs
   let wiggler = radialWiggler wigglePower
   waterPetals :: [Petal] <-
-    sequence (map (flatWaterColour 0.3 layerCount wiggler) petals) >>=
+    sequence (map (flatWaterColour 0.15 layerCount wiggler) petals) >>=
     return . concat
   return waterPetals
+
+data NoiseWalker = NoiseWalker
+  { _noiseWalkerScale :: Double
+  , _noiseWalkerStep :: Double
+  }
+
+mkNoiseWalker :: Generate NoiseWalker
+mkNoiseWalker = do
+  scale <- sampleRVar $ uniform 1000 5000
+  step <- sampleRVar $ uniform 0.1 4.0
+  return $ NoiseWalker scale step
+
+stepNoiseWalker ::
+     V2 Double -> Int -> NoiseWalker -> Generate [(Double, V2 Double)]
+stepNoiseWalker start n walker = do
+  (_, path, _) <-
+    iterateMaybeM (_stepNoiseWalker start) (n, [], walker) >>= return . last
+  return path
+
+noiseWalkerDir :: NoiseWalker -> V2 Double -> Generate Double
+noiseWalkerDir (NoiseWalker scale _) (V2 x y) = do
+  sample <- noiseSample $ V3 (x / scale) (y / scale) 0.4
+  return $ (2 *) . (pi *) $ abs sample
+
+_stepNoiseWalker ::
+     V2 Double
+  -> (Int, [(Double, V2 Double)], NoiseWalker)
+  -> Generate (Maybe (Int, [(Double, V2 Double)], NoiseWalker))
+_stepNoiseWalker start (0, _, _) = pure Nothing
+_stepNoiseWalker start (left, path, w@(NoiseWalker _ step)) = do
+  let last@(V2 x y) =
+        if null path
+          then start
+          else snd $ head path
+  theta <- noiseWalkerDir w last
+  let next = circumPoint last theta step
+  return $ Just (left - 1, (theta, next) : path, w)
+
+squigglyPath :: Int -> V2 Double -> Generate Line
+squigglyPath n start = do
+  path <- mkNoiseWalker >>= \w -> stepNoiseWalker start (max n 2) w
+  return $ fromJust $ mkLine $ V.fromList $ map snd path
+
+intoSpawnPoints ::
+     (Double -> Generate Double)
+  -> (Int -> Generate Double)
+  -> [(Double, V2 Double)]
+  -> Generate [PetalSpawnPoint]
+intoSpawnPoints thicknessF spreadF path = do
+  let enumerated = zip path [0 ..]
+  let mkSpawnPoint ((d, p), i) = do
+        spread <- spreadF i
+        thickness <- thicknessF spread
+        side :: Int <- sampleRVar $ uniform 1 2
+        let theta =
+              if side == 1
+                then d + (pi / 2)
+                else d - (pi / 2)
+        theta' <- sampleRVar $ normal 0 (pi / 5) >>= return . (+ theta)
+        let point = circumPoint p theta spread
+        return $ PetalSpawnPoint point (theta' - pi) thickness
+  sequence $ map mkSpawnPoint $ zip path [0 ..]
 
 scene :: Generate (Render ())
 scene = do
   World {..} <- asks world
-  center <- centerPoint
-  simplePalette <-
-    randElem $ V.fromList [mote, castle, metroid, gurken, redPalette]
-  layerCount <- sampleRVar $ uniform 3 6
-  petal <- mkTHColours simplePalette >>= gridPetals layerCount
+  frame <- fullFrame
+  let randSimplePalette =
+        randElem $ V.fromList [monoPastelBlue, monoPastelRed, monoPastelBlue]
+  basePalette <- randSimplePalette
+  let spreadF i = return $ (fromIntegral i / 100) * 30
+  let thicknessF s = return $ 20 - s
+  spawnPaths <-
+    sequence $
+    map
+      (const $ do
+         w <- mkNoiseWalker
+         start <- spatialSample frame
+         path <- stepNoiseWalker start 400 w
+         intoSpawnPoints thicknessF spreadF path)
+      [0 .. 10]
+  palettes <-
+    sequence $ map (const $ randSimplePalette >>= mkTHColours) spawnPaths
+  layerCount <- sampleRVar $ uniform 5 15
+  petalSets <-
+    sequence $
+    map (\(palette, path) -> waterPetals layerCount path palette) $
+    zip palettes spawnPaths
   return $ do
-    setColour $ bgColour simplePalette
+    setColour $ bgColour basePalette
     rectangle 0 0 width height
     fill
-    foldr1 (>>) $ map ((>> fill) . draw) petal
+    foldr1 (>>) $ map ((>> fill) . draw) $ concat petalSets
     return ()
 
 main :: IO ()
--- src/Generate/Colour/SimplePalette.hs
+++ src/Generate/Colour/SimplePalette.hs
@@ -8,6 +8,10 @@ module Generate.Colour.SimplePalette
   , metroid
   , gurken
   , redPalette
+  , jhoto
+  , monoPastelRed
+  , monoPastelPurple
+  , monoPastelBlue
   ) where
 
 import Data.Colour.SRGB
@@ -25,6 +29,18 @@ mkSimplePalette bg fgs =
   SimplePalette (hexcolour bg) $ V.fromList $ map hexcolour fgs
 
 redPalette = mkSimplePalette "F8F5EB" ["AD4749", "F8F5EB", "BFAAB9", "AF849B"]
+
+monoPastelRed =
+  mkSimplePalette "FFAAAA" ["D46A6A", "AA3939", "801515", "550000"]
+
+monoPastelPurple =
+  mkSimplePalette "C17BC1" ["9F4B9F", "892E89", "751975", "5D085D"]
+
+monoPastelBlue =
+  mkSimplePalette "96DEDE" ["61C0C0", "3BA2A2", "228F8F", "0C7A7A"]
+
+jhoto =
+  mkSimplePalette "F5F6ED" ["D32E40", "D77C32", "D5C06D", "0FAE7B", "21948B"]
 
 castle =
   mkSimplePalette "FFFFFF" ["C74894", "FDDCB7", "F75856", "66AA79", "07444D"]
